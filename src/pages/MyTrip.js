f93f609b3b66a0f99cc2c7e5416ce76ae037d3dc
Justin Kahrs <justin@justinkahrs.com>
2017-12-03 11:34:07 -0500

    added map

--- src/pages/MyTrip.js
+++ src/pages/MyTrip.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 
-const MyTrip = () => <div>My Trip</div>;
+const markerStyle = {
+  color: 'white',
+  backgroundColor: 'lightblue',
+  height: 50,
+  width: 50,
+  fontSize: '28px',
+  borderRadius: '50%',
+};
 
-export default MyTrip;
+class SimpleMap extends React.Component {
+  static defaultProps = {
+    center: { lat: 39.165609, lng: -86.534238 },
+    zoom: 17,
+  };
+
+  render() {
+    return (
+      <div style={{ width: '100%', height: '700px' }}>
+        <GoogleMapReact defaultCenter={this.props.center} defaultZoom={this.props.zoom}>
+          <div style={markerStyle} lat={39.165609} lng={-86.534238}>
+            JK
+          </div>
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
